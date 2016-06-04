1671e6a3fcf0e55789d47ad075ea7d4fd270db91
Richard Garner <rich@richgarner.net>
2016-06-04 16:08:12 +1000

    fixed up the state param comparison

--- my_modules/linkedin-client.js
+++ my_modules/linkedin-client.js
@@ -6,22 +6,21 @@ module.exports.create = function(options) {
 	var client_id     = options.client_id;
 	var client_secret = options.client_secret;
 	var redirect_uri  = options.redirect_uri;
-	var state         = options.state;
 	var scope         = options.scope;
 
 	return {
-		_state: {},
 		hasAccessToken: function() {
-			return this._state.auth_token;
+			return this.auth_token;
 		},
 		needsNewToken: function() {
-			return !this.hasAccessToken() && (Date.now() < _state.auth_token_expiry);
+			return !this.hasAccessToken() && (Date.now() >= this.auth_token_expiry - 60);
 		},
 		requestAuthorisation: function(http_response, state) {
+			this._last_state_string = state;
 			return http_response.redirect(
 				'https://www.linkedin.com/oauth/v2/authorization?' + querystring.stringify({
 					client_id: client_id,
-					scope: scope,
+					// scope: scope,
 					redirect_uri: redirect_uri,
 					response_type: 'code',
 					state: state
@@ -39,9 +38,10 @@ module.exports.create = function(options) {
 				error = Error('Linkedin.acceptAuthCode(http_request): ' + http_request.query.error + ": " + http_request.query.error_description);
 			}
 
-			if(http_request.query.state != state) {
+			if(http_request.query.state != this._last_state_string) {
 				error = Error("Linkedin.acceptAuthCode(http_request): http_request.query.state does not match original request's state");
 			}
+			delete this._last_state_string;
 
 			if(!http_request.query.code) {
 				error = Error("Linkedin.acceptAuthCode(http_request): there is no auth code");
@@ -71,8 +71,8 @@ module.exports.create = function(options) {
 					var json = JSON.parse(data);
 					console.log("ACCESS TOKEN", json.access_token);
 					console.log("...expires in", json.expires_in);
-					this._state.access_token = json.access_token;
-					this._state.auth_token_expiry = Date.now() + json.expires_in;
+					this.access_token = json.access_token;
+					this.auth_token_expiry = Date.now() + json.expires_in;
 					callback();
 				})			
 			});
@@ -104,7 +104,7 @@ module.exports.create = function(options) {
 			// Authorization: Bearer AQXdSP_W41_UPs5ioT_t8HESyODB4FqbkJ8LrV_5mff4gPODzOYR
 			// res.setHeader('Host', 'api.linkedin.com');
 			// res.setHeader('Connection', 'Keep-Alive');
-			// res.setHeader('Authorization', 'Bearer ' + this._state.access_token);
+			// res.setHeader('Authorization', 'Bearer ' + this.access_token);
 		}
 	}
 }
