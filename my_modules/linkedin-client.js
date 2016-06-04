24b53560912a4a2f65e44035e302e61c4e721cda
Richard Garner <rich@richgarner.net>
2016-06-04 15:15:06 +1000

    Refactored linkedin stuff out into a new module. Haven't tested it yet.

---
+++ my_modules/linkedin-client.js
@@ -0,0 +1,109 @@
+var https = require('https');
+
+exports.Linkedin.create = function(options) {
+
+	var client_id     = options.client_id;
+	var client_secret = options.client_secret;
+	var redirect_uri  = options.redirect_uri;
+	var state         = options.state;
+	var scope         = options.scope;
+
+	return {
+		_state: {},
+		hasAccessToken: function() {
+			return this._state.auth_token;
+		},
+		needsNewToken: function() {
+			return !this.hasAccessToken() && (Date.now() < _state.auth_token_expiry);
+		},
+		requestAuthorisation: function(http_response, state) {
+			return http_response.redirect(
+				'https://www.linkedin.com/oauth/v2/authorization?' + querystring.stringify({
+					client_id: client_id,
+					scope: scope,
+					redirect_uri: redirect_uri,
+					response_type: 'code',
+					state: state
+				})
+			);
+		},
+		acceptAuthCode: function(http_request, callback) {
+			
+			var error;
+			if(req.method != 'GET') {
+				error = Error('Linkedin.acceptAuthCode(http_request): http_request.method must be a GET')
+			}
+
+			if(req.query.error) {
+				error = Error('Linkedin.acceptAuthCode(http_request): ' + req.query.error + ": " + req.query.error_description);
+			}
+
+			if(req.query.state != state) {
+				error = Error("Linkedin.acceptAuthCode(http_request): http_request.query.state does not match original request's state");
+			}
+
+			if(!req.query.code) {
+				error = Error("Linkedin.acceptAuthCode(http_request): there is no auth code");
+			}
+
+			callback(error, req.query.code);
+
+		},
+		// Step 3 — Exchange Authorization Code for an Access Token
+		requestAccessToken: function(auth_code, callback) {
+
+			var options = {
+				hostname: 'www.linkedin.com',
+				port: 443,
+				path: '/oauth/v2/accessToken',
+				method: 'POST'
+			};
+
+			var access_token_request = https.request(options, (access_token_response) => {
+
+				var data = '';
+				access_token_response.on('data', (chunk) => {
+					data += chunk;
+				});
+
+				access_token_response.on('end', () => {
+					var json = JSON.parse(data);
+					console.log("ACCESS TOKEN", json.access_token);
+					console.log("...expires in", json.expires_in);
+					this._state.access_token = json.access_token;
+					this._state.auth_token_expiry = Date.now() + json.expires_in;
+					callback();
+				})			
+			});
+
+			access_token_request.setHeader('Content-Type', 'application/x-www-form-urlencoded');
+			access_token_request.setHeader('Host', 'www.linkedin.com');
+			access_token_request.setHeader('User-Agent', 'curl/7.43.0');
+			access_token_request.setHeader('Accept', '*/*');
+
+			access_token_request.on('error', (e) => {
+				callback(e);
+			})
+
+			var url_encode_body = querystring.stringify({
+				grant_type: 'authorization_code',
+				code: auth_code,
+				redirect_uri: redirect_uri,
+				client_id: client_id,
+				client_secret: client_secret
+			})
+
+			access_token_request.write(url_encode_body);
+			access_token_request.end();
+		},
+		_request: function() {
+			// GET /v1/people/~ HTTP/1.1
+			// Host: api.linkedin.com
+			// Connection: Keep-Alive
+			// Authorization: Bearer AQXdSP_W41_UPs5ioT_t8HESyODB4FqbkJ8LrV_5mff4gPODzOYR
+			// res.setHeader('Host', 'api.linkedin.com');
+			// res.setHeader('Connection', 'Keep-Alive');
+			// res.setHeader('Authorization', 'Bearer ' + this._state.access_token);
+		}
+	}
+}
--- server.js
+++ server.js
@@ -3,25 +3,29 @@
 // Server onfiguration params
 const port = 80;
 
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const session     = require('express-session');
-const _           = require('underscore');
-const querystring = require('querystring');
-const https       = require('https');
-const crypto      = require('cryptp');
-const app         = express();
+const express               = require('express');
+const bodyParser            = require('body-parser');
+const session               = require('express-session');
+const _                     = require('underscore');
+const querystring           = require('querystring');
+const crypto                = require('crypto');
+const app                   = express();
+const LinkedinClientFactory = express('./linkedin-client.js');
 
 // Set up handlebars as the view engine
 const exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
-// Keeps track of the current state of the app
-var server_state = {};
+// Set up the Linkedin client to work with the application we've already set up
+var linkedin_client = LinkedinClientFactory.create({
+	client_id: '75r0sg4teeoy7e',
+	client_secret: 'AXoszgsSSCVkiZzq',
+	redirect_uri: 'http://ec2-52-197-31-112.ap-northeast-1.compute.amazonaws.com/oauth/linkedin'
+});
 
 ////////////////////////////
-// BASIC MIDDLEWARE STUFF //
+// basic middleware stuff //
 ////////////////////////////
 
 // The middleware to handle sessions
@@ -59,146 +63,58 @@ app.use((req, res, next) => {
 	next();
 });
 
-////////////////////////////////
-// LINKEDIN MIDDLE WARE STUFF //
-////////////////////////////////
-
-// From the LinkedIn API docs for OAuth 2
-// https://developer.linkedin.com/docs/oauth2
-// This is the application that has been set up in LinkedIn
-
-const client_id = '75r0sg4teeoy7e';
-const client_secret = 'AXoszgsSSCVkiZzq';
-const redirect_uri = 'http://ec2-52-197-31-112.ap-northeast-1.compute.amazonaws.com/oauth/linkedin';
-
-// The state is used by OAuth 2 to improve security
-var state;
-crypto.randomBytes(256, (err, buffer) => {
-	state = crypto.createHash('sha256').update(buffer).digest('hex');
-});
-
-// Step 2 — Request an Authorization Code
-var query_params = {
-	client_id: client_id,
-	// scope: 'asd',
-	redirect_uri: redirect_uri,
-	response_type: 'code',
-	state: state
-};
+///////////////////////////////
+// Linkedin middleware stuff //
+///////////////////////////////
 
 // Redirects the user to LinkedIn to authorise this application if there is no ACCESS TOKEN
 app.use((req, res, next) => {
 
-	if(req.path != '/login' && req.path != '/oauth/linkedin') {
-		// If we don't have an auth token to talk to the Linkein app,
-		// then get the user to authorize us on their behalf.
-		if(!server_state.access_token) {
-			return res.redirect('https://www.linkedin.com/oauth/v2/authorization?' + querystring.stringify(query_params));
-		} else {
-			// GET /v1/people/~ HTTP/1.1
-			// Host: api.linkedin.com
-			// Connection: Keep-Alive
-			// Authorization: Bearer AQXdSP_W41_UPs5ioT_t8HESyODB4FqbkJ8LrV_5mff4gPODzOYR
-			// res.setHeader('Host', 'api.linkedin.com');
-			// res.setHeader('Connection', 'Keep-Alive');
-			// res.setHeader('Authorization', 'Bearer ' + server_state.access_token);
-		}
+	if(req.path != '/login' && req.path != '/oauth/linkedin' && !linkedin_client.hasAccessToken()) {
+		// The state is used by OAuth 2 to improve security
+		crypto.randomBytes(256, (err, buffer) => {
+			// If we don't have an access token to talk to the Linkein app,
+			// then get the user to authorize us on their behalf.
+			var state = crypto.createHash('sha256').update(buffer).digest('hex');
+			return linkedin_client.requestAuthorisation(res, state);
+		});
+	} else {
+		next();
 	}
-	next();
 
 });
 
+////////////////////////////////////////
+// The main pages for the application //
+////////////////////////////////////////
+
+// This is the resource that the Linkedin API directs the user
+// back to once it's approvaed the user's authorisation to let
+// this application access it's account.
 app.get('/oauth/linkedin', (req, res) => {
-	// http://music.richgarner.net/
-	// ?code=AQQI9dbaDrUAmgLU8Sn0ljy1FC0M-_T9po7xViSwUVRXHMt1K5tPR_J2rNF3M4TQ3l73B4qkb5U8GEclAQ4PZloJFmGZ0BiS446Vrin5AjVQeUfeoBI
-	// &state=fjseeoo32rv
-	console.log("REQ: ", req);
-	for(var i in req) {
-		if(i == 'query') {
-			console.log(i, req[i]);
+
+	linkedin_client.acceptAuthCode(req, (error, auth_code) => {
+		if(error) {
+			throw error;
 		}
-	}
-	console.log("REQ.method: ", req.method);
-	console.log("REQ.QUERY: ", req.query);
-	if(req.query.error) {
-		throw Error(req.query.error + ": " + req.query.error_description);
-	}
-
-	if(req.query.state != state) {
-		throw Error("State is wrong!");
-	}
-
-	if(!req.query.code) {
-		throw Error("There is no AUTH CODE!");
-	}
-
-	// Step 3 — Exchange Authorization Code for an Access Token
-	var options = {
-		hostname: 'www.linkedin.com',
-		port: 443,
-		path: '/oauth/v2/accessToken',
-		method: 'POST'
-		// headers: {
-
-		// }
-	};
-	// linkedin.aquireAccessToken((err, token) => {
-	// 		console.log(token);
-	// 	 	res.redirect('/');
-	// })
-	var access_token_request = https.request(options, (access_token_response) => {
-		console.log('statusCode: ', access_token_response.statusCode);
-		console.log('headers: ', access_token_response.headers);
-		var data = '';
-		access_token_response.on('data', (chunk) => {
-			data += chunk;
-			console.log(chunk);
-		});
-		access_token_response.on('end', () => {
-			console.log('Response is done');
-			var json = JSON.parse(data);
-			console.log(json);
-			console.log("ACCESS TOKEN", json.access_token);
-			console.log("...expires in", json.expires_in);
-			server_state.access_token = json.access_token;
-			server_state.expires_in = json.expires_in;
+		linkedin_client.requestAccessToken(auth_code, (error) => {
 			res.redirect('/');
-		})
+		});
 	});
-
-	access_token_request.setHeader('Content-Type', 'application/x-www-form-urlencoded');
-	access_token_request.setHeader('Host', 'www.linkedin.com');
-	access_token_request.setHeader('User-Agent', 'curl/7.43.0');
-	access_token_request.setHeader('Accept', '*/*');
-
-	access_token_request.on('error', (e) => {
-		console.error(e.message);
-	})
-
-	console.log("About to POST for access_token");
-	var url_encode_body = querystring.stringify({
-		grant_type: 'authorization_code',
-		code: req.query.code,
-		redirect_uri: redirect_uri,
-		client_id: client_id,
-		client_secret: client_secret,
-	})
-	console.log("URL_ENCOIDE_BODY: ", url_encode_body);
-	access_token_request.write(url_encode_body);
-	access_token_request.end();
 
 });
 
 // TODO: Use HTTPS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
+// Shows the main messenger page
 app.get('/', (req, res) => {
 	var sess = req.session;
-	console.log("AUTH TOKEN = ", server_state.access_token)
 	res.render('messenger', {
 		message: 'You are already logged in: '+ sess.username + ', ' + sess.password
 	});
 });
 
+// Handles POSTS to the main messenger page
 app.post('/', (req, res) => {
 
 	// get the POST params
@@ -213,14 +129,15 @@ app.post('/', (req, res) => {
 		message: log
 	});
 
-
 });
 
+// The login page. Obvs.
 app.get('/login', function(req, res) {
 	// render the login page
 	res.render('login');
 });
 
+// Handles login requests. Obvs.
 app.post('/login', function(req, res) {
 
 	// set the username and password on the session
@@ -232,6 +149,7 @@ app.post('/login', function(req, res) {
 	res.redirect('/');
 });
 
+// Bind the server and start listening
 app.listen(port, function() {
 	console.log('listening on port port');
 });
